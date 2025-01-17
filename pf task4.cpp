405b969153ddddbae41c74cb5eacba9b328710b5
Dilawer-alee <dilawer2382005@gmail.com>
2025-01-17 23:42:04 +0500

    code push

---
+++ pf task4.cpp
@@ -0,0 +1,37 @@
+
+//task4
+//1                                                                                                             
+//22                                                                                                      
+//333                                                                                                         
+//4444                                                                                                         
+//55555 
+
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//	
+//for(int i=1; i<6; i++){
+//	for(int j=1; j<=i; j++){
+//			cout<<i;
+//	
+//	}
+//	cout<<endl;
+//}
+//	}
+	
+	
+	//task5
+	
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	for(int i=5; i>=1; i--){
+		for(int j=1; j<=i; j++){
+			cout<<j;
+		}
+		cout<<endl;
+	}
+} 
